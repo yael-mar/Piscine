dcae31bccb576ddcf469527aefc792eac590188b
root <root@DESKTOP-MLML3RD.localdomain>
2026-03-04 07:51:01 +0100

    Second Commit

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yael-mar <elmardi.yas@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/04 07:37:51 by yael-mar          #+#    #+#             */
-/*   Updated: 2026/03/04 07:37:51 by yael-mar         ###   ########.fr       */
+/*   Created: 2026/03/04 07:45:44 by yael-mar          #+#    #+#             */
+/*   Updated: 2026/03/04 07:45:44 by yael-mar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_alphabet()
+int ft_print_reverse_alphabet()
 {
     char c;
-
-    c = 'a';
-    while (c <= 'z')
+    
+    c = 'z';
+    while(c >= 'a')
     {
-        write (1, &c, 1);
-        c++;
+        write(1, &c, 1);
+        c--;
     }
+}
+
+int main()
+{
+    ft_print_reverse_alphabet();
 }
