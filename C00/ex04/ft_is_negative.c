90dccdf0868b50461b68072c7c6473d81f20071d
root <root@DESKTOP-MLML3RD.localdomain>
2026-03-04 08:04:37 +0100

    Piscine

---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yael-mar <elmardi.yas@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 07:58:25 by yael-mar          #+#    #+#             */
+/*   Updated: 2026/03/04 07:58:25 by yael-mar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_is_negative(int n)
+{
+    char negative;
+    char positive;
+    
+    negative = 'N';
+    positive = 'P';
+    if (n < 0)
+    {
+        write (1, &negative, 1);
+    }
+    else
+    {
+        write (1, &positive, 1);
+    }
+}
